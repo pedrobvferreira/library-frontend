7c7d316592444cdb7696ecbfca1183ddabb92862
pedrobvferreira <pedrobvf@hotmail.com>
2024-06-19 23:48:14 +0100

    006-fix buttons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,11 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { InjectionToken } from '@angular/core';
-import { environment } from '../environments/environment.dev';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
 
-export const APP_CONFIG = new InjectionToken('app.config');
+import { appRoutes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    importProvidersFrom(HttpClientModule),
-    provideRouter(appRoutes),
-    { provide: 'API_URL', useValue: environment.apiUrl }
-  ],
+  providers: [provideRouter(appRoutes), provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch())]
 };
